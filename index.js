402005e00d7696bfbfc5218568eb777856cb9745
AidenNateLuk <idanathanieluk@gmail.com>
2024-04-21 15:45:04 +0300

    adjusted min-width sizes to match the original source

--- index.js
+++ index.js
@@ -3,20 +3,39 @@ const ctx = document.getElementById("myChart");
 new Chart(ctx, {
   type: "bar",
   data: {
-    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    labels: ["mon", "tue", "wed", "thu", "fri", "sat", "sun"],
+    color: "hsl(28, 10%, 53%)",
     datasets: [
       {
-        label: "# of Votes",
-        data: [12, 19, 3, 5, 2, 3],
-        borderWidth: 1,
+        label: "",
+        data: [7, 12, 19, 11, 8, 14, 10],
+        backgroundColor: [
+          " hsl(10, 79%, 65%)",
+          " hsl(10, 79%, 65%)",
+          "hsl(186, 34%, 60%)",
+          " hsl(10, 79%, 65%)",
+          " hsl(10, 79%, 65%)",
+          " hsl(10, 79%, 65%)",
+        ],
+        borderWidth: 0,
+        borderRadius: 5,
+        hidden: true,
       },
     ],
   },
   options: {
     scales: {
-      y: {
-        beginAtZero: true,
+      x: {
+        grid: {
+          display: false,
+        },
       },
+      y: {
+        display: false,
+      },
+    },
+    legend: {
+      display: false,
     },
   },
 });
